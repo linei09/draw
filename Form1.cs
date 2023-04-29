73719cc3908e739a6f48b69cf66602642ce63290
HuyThoiii <127771898+trum2015vungtau@users.noreply.github.com>
2023-04-29 12:08:17 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bm);
+            g.Clear(Color.White);
+            pictureBox1.Image = bm;
+        }
+
+        Bitmap bm;
+        Graphics g;
+        bool paint = false;
+        Point px, py;
+        Pen p1 = new Pen(Color.Black, 1);
+        Pen p2 = new Pen(Color.Black, 5);
+        Pen p3 = new Pen(Color.Black, 10);
+        Pen eraser1 = new Pen(Color.White,10);
+        Pen eraser2 = new Pen(Color.White,30);
+        Pen eraser3 = new Pen(Color.White,50);
+        ColorDialog cd = new ColorDialog();
+        Color new_color;
+        int index,x,y,cX,cY,sX,sY;
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.Filter = "PNG (*.png) | *.png |PDF(*.pdf) | *.pdf | All files(*.*) | *.* ";
+            if(openFileDialog.ShowDialog()==DialogResult.OK)
+            {
+                pictureBox1.Image=Image.FromFile(openFileDialog.FileName);
+            }
+           
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Save";
+            saveFileDialog.ShowDialog();
+            
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog_SaveAs = new SaveFileDialog();
+            saveFileDialog_SaveAs.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult Exit = MessageBox.Show("Want to close the program?","Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if(Exit == DialogResult.Yes) { Close(); }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Eraser_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Eraser_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Eraser_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Eraser_MouseUp(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Eraser_Click(object sender, EventArgs e)
+        {
+            panel3.Visible = true;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint = true;
+            py = e.Location;
+
+            cX = e.X;
+            cY = e.Y;
+           
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (paint)
+            {
+                if (index == 1)
+                {
+                    px = e.Location;
+                    g.DrawLine(eraser1, px, py);
+                    py = px;
+                }
+                if (index == 2)
+                {
+                    px = e.Location;
+                    g.DrawLine(eraser2, px, py);
+                    py = px;
+                }
+                if (index == 3)
+                {
+                    px = e.Location;
+                    g.DrawLine(eraser3, px, py);
+                    py = px;
+                }
+                if (index == 4)
+                {
+                    px = e.Location;
+                    g.DrawLine(p1, px, py);
+                    py = px;
+                }
+                if (index == 5)
+                {
+                    px = e.Location;
+                    g.DrawLine(p2, px, py);
+                    py = px;
+                }
+                if (index == 6)
+                {
+                    px = e.Location;
+                    g.DrawLine(p3, px, py);
+                    py = px;
+                }
+              
+
+            }
+            pictureBox1.Refresh();
+            x = e.X; y = e.Y;
+            sX = e.X - cX;
+            sY = e.Y - cY;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            paint = false;
+            sX = e.X - cX;
+            sY = e.Y - cY;
+            double size = 100.00;
+
+            if (index == 10)
+                {
+                    g.DrawEllipse(p1,cX,cY,sX,sY);
+                }
+              if(index == 11)
+            {
+                g.DrawRectangle(p1,cX,cY,sX,sY);
+            }
+            //  if(index==12)
+            //{
+            //    double angle = 2 * Math.PI / 3; // Góc giữa các cạnh của tam giác đều
+            //    double[] x = new double[3];
+            //    double[] y = new double[3];
+
+            //    for (int i = 0; i < 3; i++)
+            //    {
+                    
+            //        x[i] = cX + size * Math.Cos(i * angle);
+            //        y[i] = cY + size * Math.Sin(i * angle);
+            //        Point[] points = new Point[] { new Point((int)x[0], (int)y[0]), new Point((int)x[1], (int)y[1]), new Point((int)x[2], (int)y[2]) };
+            //        g.DrawPolygon(p1, points);
+
+
+            //    }
+
+            //}
+
+        }
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void back_eraser_Click(object sender, EventArgs e)
+        {
+            panel3.Visible = false;
+        }
+
+        private void size_eraser_1_Click(object sender, EventArgs e)
+        {
+            index = 1;
+        }
+
+        private void size_eraser_2_Click(object sender, EventArgs e)
+        {
+            index = 2;
+        }
+
+        private void size_eraser_3_Click(object sender, EventArgs e)
+        {
+            index = 3;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel4.Visible = false;
+        }
+
+        private void Polygons_Click(object sender, EventArgs e)
+        {
+            panel4.Visible = true;
+        }
+
+        private void _circle_Click(object sender, EventArgs e)
+        {
+            index = 10;
+        }
+
+        private void back_pen_Click(object sender, EventArgs e)
+        {
+            panel5.Visible = false;
+        }
+
+        private void _retangle_Click(object sender, EventArgs e)
+        {
+            index = 11;
+        }
+
+        private void _polygons_Click(object sender, EventArgs e)
+        {
+            index = 12;
+        }
+
+        private void size_pen_1_Click(object sender, EventArgs e)
+        {
+            index = 4;
+        }
+
+        private void size_pen_2_Click(object sender, EventArgs e)
+        {
+            index = 5;
+        }
+
+        private void size_pen_3_Click(object sender, EventArgs e)
+        {
+            index = 6;
+        }
+
+        private void back_color_Click(object sender, EventArgs e)
+        {
+            panel6.Visible = false;
+        }
+
+        private void _Color_Click(object sender, EventArgs e)
+        {
+            cd.ShowDialog();
+            new_color=cd.Color;
+            _Color.BackColor = cd.Color;
+            p1.Color = cd.Color;
+            p2.Color = cd.Color;
+            p3.Color = cd.Color;
+        }
+
+        private void color_red_Click(object sender, EventArgs e)
+        {
+            index = 7;
+        }
+
+        private void color_blue_Click(object sender, EventArgs e)
+        {
+            index = 8;
+        }
+
+        private void color_yellow_Click(object sender, EventArgs e)
+        {
+            index = 9;
+        }
+
+        private void Pen_Click(object sender, EventArgs e)
+        {
+            panel5.Visible = true;
+        }
+    }
+}
